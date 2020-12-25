22e712c7a1458163cbb307ad00a83aa776cd17ef
naughty-melon <zzdd912912@163.com>
2020-12-25 17:33:46 +0800

    fix:添加样式文件

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,21 @@
   <div id="app">
     <!-- 根路由出口 -->
     <router-view/>
+    <p class="text">hello</p>
   </div>
 </template>
 
-<style lang="scss">
+<script lang="ts">
+import Vue from 'vue'
 
+export default Vue.extend({
+  name: 'App'
+})
+</script>
+
+<style lang="scss" scoped>
+@import "~@/styles/variables.scss";
+.text {
+  color: $success-color
+}
 </style>
