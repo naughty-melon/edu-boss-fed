c775eb5e9e34647a8eb8decc2d5b779e82d92aa3
weia <zzdd912912@163.com>
2021-01-13 11:36:34 +0800

    fix:update

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -57,7 +57,7 @@ export default Vue.extend({
   methods: {
     async onSubmit () {
       try {
-        // 1.表单验证
+        // 1.表单验证18201288771
         await (this.$refs.form as Form).validate()
         // 登录按钮loading
         this.isLoginLoading = true
@@ -72,9 +72,7 @@ export default Vue.extend({
           this.$store.commit('setUser', data.content)
           // 2.然后再访问需要登录的页面的时候判断有没有登录状态（路由拦截器）
           // 成功：跳转到首页
-          this.$router.push({
-            name: 'home'
-          })
+          this.$router.push(this.$route.query.redirect as string || '/')
           this.$message.success('登录成功')
         }
       } catch (err) {
